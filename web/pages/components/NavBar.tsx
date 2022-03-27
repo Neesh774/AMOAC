447cbc27ad4e5a0019205fd0505d4fb23b7b343f
Neesh774 <Kanishq0106@gmail.com>
2022-03-27 00:08:55 -0400

    chakra ui

--- web/pages/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-import { Header, Title } from "@mantine/core";
-
-export default function NavBar() {
-  return (
-    <Header height={60} p="xs">
-      <Title order={1}>APUSH Me Off A Cliff</Title>
-    </Header>
-  );
-}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,8 @@
-import NavBar from "./components/NavBar";
+import Nav from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
+      <Nav />
     </div>
   );
 }
